548e7400c82da6db9a6d4e05b6734386a3eaeee1
Owen Miller <62402703+RamenRed@users.noreply.github.com>
2024-11-22 18:57:35 -0600

    Co-authored-by: Someguy45 <Someguy45@users.noreply.github.com> Converted GAN model into CNN model and simplified the initialization of model parameters. Moved training into global scope for automatic run upon program start. redefined helper functions to consistently reshape and resize numpy arrays used as intermediaries between images and tensors. Added some test code at the end for resting run_model().

--- app/defaker_backend.py
+++ app/defaker_backend.py
@@ -2,12 +2,9 @@ from torch import *
 import torch
 import torch.nn as nn
 import torch.nn.parallel
-import torch.optim as opt
+import torch.nn.functional as F
+import torch.optim as optim
 from torchvision import *
-import torch.utils.data as u_data
-import torchvision.datasets as tv_dset
-import torchvision.transforms as tv_transforms
-import torchvision.utils as vutils
 import numpy as np
 import random
 from PIL import Image
@@ -15,19 +12,15 @@ import os
 
 import sys
 
-MY_UTILS_PATH = 'F:/GitHub/Deepfake-Defaker/app'
+MY_UTILS_PATH = './app'
 if not MY_UTILS_PATH in sys.path:
     sys.path.append(MY_UTILS_PATH)
 
 
-#import app.defaker_api as df_api
-#import torch.utils.data.dataloader as torchDL
+#import torch.utils.data.dataloader
 from torch.utils.data import DataLoader
-#import torch.utils.data.dataset as torchDS
+#import torch.utils.data.dataset
 from torch.utils.data import Dataset
-
-
-# With help from https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html
 
 # =======================================================
 #                 Initial Parameters
@@ -98,11 +91,10 @@ def model_probability_opinion(opinions: list): # Used to calculate how many time
         cycle_count += 1
     return (dfd_average/len(opinions)) * 100
 
-def gan_logic(dfg, dfd):
-    pass
-
 def img_to_tensor(image):
     t_form = np.array(image)
+    t_form.resize(65536)
+    t_form.reshape(256, 256)
     t_img = torch.from_numpy(t_form)
     return t_img
 
@@ -146,8 +138,8 @@ class images_data(Dataset):
 dataset = images_data(image_arrays, device=device)
 dataloader = DataLoader(dataset, batch_size=1, shuffle=True, num_workers=4)
 
-x_entropy = nn.BCELoss()
 
+'''
 def weights_init(weights_inst):
     c_name = weights_inst.__class__.__name__
     if c_name.find('Conv') != -1:
@@ -155,59 +147,29 @@ def weights_init(weights_inst):
     elif c_name.find('BatchNorm') != -1:
         nn.init.normal_(weights_inst.weight.data, 1.0, 0.02)
         nn.init.constant_(weights_inst.bias.data, 0)
+'''
 # =======================================================
-#                      Models
+#                      Model
 # =======================================================
 
-class Defaker_generator(nn.Module):
+class Defaker_CNN(nn.Module):
     def __init__(self):
-        super(Defaker_generator, self).__init__()
-        self.loss_fn = nn.BCELoss()
-        self.model = nn.Sequential(
-            nn.ConvTranspose2d(100, 64 * 8, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(64 * 8),
-            nn.LeakyReLU(True),
-            nn.ConvTranspose2d(64 * 8, 64 * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(64 * 4),
-            nn.LeakyReLU(True),
-            nn.ConvTranspose2d(64 * 4, 64 * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(64 * 2),
-            nn.LeakyReLU(True),
-            nn.ConvTranspose2d(64 * 2, 64, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(True),
-            nn.ConvTranspose2d(64, 3, 4, 2, 1, bias=False),
-            nn.Tanh()
-        )
-
-    def loss(self, fake):
-        return self.loss_fn(torch.ones_like(fake), fake)
-    
-    def forward(self, gen_in):
-        return self.model(gen_in)
-
-class Defaker_discriminator(nn.Module):
-    def __init__(self):
-        super(Defaker_discriminator, self).__init__()
-        self.loss_fn = nn.BCELoss()
-        self.model = nn.Sequential(
-            nn.Conv2d(3, 64, 4, 2, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(64, 64 * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(64 * 2),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(64 * 2, 64 * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(64 * 4),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(64 * 4, 64 * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(64 * 8),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(8 * 8, 1, 4, 1, 0, bias=False),
-            nn.Sigmoid()
-        )
-    
+        super(Defaker_CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+        
     def forward(self, discrim_in):
-        return self.model(discrim_in)
+        discrim_in = self.pool(F.relu(self.conv1(discrim_in)))
+        discrim_in = self.pool(F.relu(self.conv2(discrim_in)))
+        discrim_in = discrim_in.view(-1, 16 * 5 * 5)
+        discrim_in = F.relu(self.fc1(discrim_in))
+        discrim_in = F.relu(self.fc2(discrim_in))
+        discrim_in = self.fc3(discrim_in)
+        return discrim_in
     
     def loss(self, real, fake):
 
@@ -220,123 +182,61 @@ class Defaker_discriminator(nn.Module):
         total_loss = real_loss + fake_loss
         return total_loss
         
+defaker = Defaker_CNN()
 
-        
+# Setup use of multiple GPU's if present and capable
+if (device.type == "cuda") and (num_examples >1):
+        defaker = torch.nn.parallel.DistributedDataParallel(defaker)
+
+x_entropy = nn.CrossEntropyLoss()
+optimizer = optim.SGD(defaker.parameters(), lr=l_rate, momentum=0.9)  
     
-    
 
+for epoch in torch.arange(0, tot_epochs):
+    running_loss = 0
+    for i, data in enumerate(dataloader, 0):
+        inputs, labels = data
+        optimizer.zero_grad()
+        output = defaker(inputs)
+        loss = x_entropy(output, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
 
+        if i % 2000 == 1999:
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0
 
-def trainer_function(dfd: Defaker_discriminator, dfg: Defaker_generator, dataloader, optimizer_disc, optimizer_gen):
-    noise = torch.randn(max_batch_size, d_noise, 1, 1, device=device)
-    Gen_losses = []
-    Disc_losses = []
-    iters = 0
-    for epoch in range(tot_epochs):
-        for i, real_images in enumerate(dataloader):
-            
-            real_images = real_images.to(device)
-
-            #Real image batch
-            optimizer_disc.zero_grad()
-            label = torch.ones(real_images.size(0), device=device)
-            output = dfd(real_images)
-            d_loss_real = x_entropy(output, label)
-            d_loss_real.backward()
-
-            noise = torch.randn(real_images.size(0), d_noise, 1, 1, device=device)
-            fakers = dfg(noise)
-            label.fill_(0)
-            output = dfd(fakers.detach())
-
-            d_loss_fake = x_entropy(output, label)
-            d_loss_fake.backward()
-            optimizer_disc.step()
-            
-            d_loss_total = d_loss_real + d_loss_fake
-
-            #Fake image batch
-            optimizer_gen.zero_grad()
-            label.fill_(1)
-            output = dfd(fakers)
-
-            g_loss = x_entropy(output, label)
-            g_loss.backward()
-            optimizer_gen.step()
-            
-            
-
-            
-            
-
-            if iters % 50 == 0:
-                print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f'
-                % (epoch, tot_epochs, i, len(dataset.images),
-                d_loss_total.item(), g_loss.item()))
-            
-            Gen_losses.append(g_loss.item())
-            Disc_losses.append(d_loss_total.item())
-
-            if (iters % 500 == 0) or ((epoch == tot_epochs-1) and (i == len(dataset.images)-1)):
-                with torch.no_grad():
-                    fake = dfg(noise).detach().cpu()
-                dataset.images.append(vutils.make_grid(fake, padding=2, normalize=True))
-
-            iters += 1        
+print("Finished Training")
 
 # =======================================================
 
-def run_model(image):
-
-    
-
-    dfg = Defaker_generator().to(device)
-    dfd = Defaker_discriminator().to(device)
-
-    optimizer_gen = opt.Adam(dfg.parameters(), lr=l_rate)
-    optimizer_disc = opt.Adam(dfd.parameters(), lr=l_rate)
-
-    # Setup use of multiple GPU's if present and capable for Discriminator and Generator 
-    if (device.type == "cuda") and (num_examples >1):
-        dfg = torch.nn.parallel.DistributedDataParallel(dfg)
-    if (device.type == "cuda") and (num_examples >1):
-        dfd = torch.nn.parallel.DistributedDataParallel(dfd)
-
+def run_model(image): 
     # Initialize Weights
-    #dfg.apply(weights_init)
-    #dfd.apply(weights_init)
+
+    #Display Architecture in terminal for testing purposes, not shown to user
+    print("Defaker_CNN Architecture:")
+    print(defaker)
+
+    # Convert image to tensor
+    tensor_img = img_to_tensor(image)
     
-    dataset = images_data(image_arrays, device=device)
-    dataloader = DataLoader(dataset, batch_size=max_batch_size, shuffle=True, num_workers=4)
-    
-
-    print("Generator Architecture:")
-    print(dfg)
-    print("\nDiscriminator Architecture:")
-    print(dfd)
-
-    noise = torch.randn(max_batch_size, d_noise, 1, 1, device=device)
-    fake_images = dfg(noise)
-    print(f"Fake Images Shape: {fake_images.shape}")
-
-    d_output = dfd(fake_images)
-    print(f"Discriminator Output Shape: {d_output.shape}")
-    print(f"Discriminator Output: {d_output}")
-
-
-    real_images = torch.randn(max_batch_size,3, 256, 256, device=device)
-    print(f"Real Images Shape: {real_images.shape}")
-
-    d_real_output = dfd(real_images)
-    
-    print(f"Real Discriminator Output Shape: {d_real_output.shape}")
-    print(f"Real Discriminator Output: {d_real_output}")
-
-    trainer_function(dfd, dfg, dataloader, optimizer_disc, optimizer_gen)
-    
-    dfd_opinions: list = [True]
+    #noise = torch.randn(max_batch_size, d_noise, 1, 1, device=device)
+    #real_images = torch.randn(max_batch_size,3, 256, 256, device=device)
+    dfd_opinions = []
+    with torch.no_grad():
+        for _ in range(0, 50):
+            opinion_value = defaker(tensor_img)
+            _, test_val = torch.max(opinion_value.data, 1)
+            if test_val.item() > 0.5:
+                dfd_opinions.append(True)
+            else:
+                dfd_opinions.append(False)
     return model_probability_opinion(dfd_opinions)
     #return "model ran without errors"
 
-test_num = run_model(image_arrays)
-print(test_num)
+if __name__ == '__main__':
+    test_img_dir = './ui/background.jpg'
+    test_img = Image.open(test_img_dir)
+    test_num = run_model(test_img)
+    print(test_num)
